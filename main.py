3619583111e806fdf9d92eb56745773b0dbe6e99
Grace Egglestone <g.egglestone@surreyschools.ca>
2022-09-23 13:40:25 -0700

    MathPractice
    Test Upload

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+    Mathematical Operators
+    + Add
+    - Subtract
+    * Multiply
+    / Divide
+    // Base Division
+    ++ Exponentiate
+    % Modulo
+'''
+
+a = 5.70
+b = 3.14
+
+print(a / b)
