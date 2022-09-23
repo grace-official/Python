32cc28c37f2c01deb9d46412fb92820e2020893f
Grace Egglestone <g.egglestone@surreyschools.ca>
2022-09-23 14:04:35 -0700

    ok
    ok

--- main.py
+++ main.py
@@ -13,3 +13,18 @@ a = 5.70
 b = 3.14
 
 print(a / b)
+
+'''
+    Relational Operators
+    == Equals
+    != Does Not Equal
+    > Greater Than
+    < Less Than
+    >= Greater Than Or Equal To
+    <=Less Than Or Equal To
+'''
+
+A = 'Apple'
+B = 'Apple'
+
+print(A == B)
