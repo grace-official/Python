c8cfb293f8065771750c0df0f53e7bdbb4bf3aa4
Grace Egglestone <g.egglestone@surreyschools.ca>
2022-09-28 14:15:03 -0700

    e
    e

--- Loops/LoopsPractice.py
+++ Loops/LoopsPractice.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+'''
+menu = {"Burger", "Fries", "Pop", "Pancakes"}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+for food in menu:
+    print(food)
+'''
 
+'''
+for num in range(99, 0, -1):
+    print(str(num) + " Bottles of beer on the wall," + str(num) + " bottles of beer, Take one down and pass it around...")
+'''
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+'''
+limit = 100
+while 1 < 2:
+    limit-=1
+    print("Hello! This will run " + str(limit) + (" times!") )
+    if(limit == 0):
+        break
 
+print("Loop Finished")
+'''
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+goodInput = False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while not goodInput:
+    userInput = int(input("1) Start Game  \n2) Load Game  \n3) Quit Game"))
+    if(userInput == 1):
+        print("Game Started!")
+        goodInput = True
+    elif(userInput == 2):
+        print("Game Loaded!")
+        goodInput = True
+    elif(userInput == 3):
+        print("Bye!")
+        goodInput = True
+    else:
+        print("Invalid Input, Please try again!")
