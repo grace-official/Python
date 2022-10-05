e5350e97dc0b7c548bc63d9781c72793ac50e0fd
Grace Egglestone <g.egglestone@surreyschools.ca>
2022-10-05 09:07:12 -0700

    e
    e

---
+++ pythonProject5/Formulas.py
@@ -0,0 +1,22 @@
+# Formulas is my module ... and thats all I need!
+
+# Functions
+def sum(numOne, numTwo):
+    return int(numOne + numTwo)
+
+def difference(numOne, numTwo):
+    return int(numOne - numTwo)
+
+
+
+# Messages is my module AND my class
+
+class Messages:
+    # Constructor - sets up the initial state of my project
+    def __init__(self):
+        print('Messages class created!')
+
+    # Methods
+    def printMessage(self):
+        print('Hello World!')
+
---
+++ pythonProject5/Runner.py
@@ -0,0 +1,9 @@
+# Python classes ... and modules!
+from Formulas import Messages
+import Formulas as F
+
+messages = Messages()
+messages.printMessage()
+
+print(F.sum(3,4))
+print(F.difference(4,6))
