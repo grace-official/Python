69ab3b15ced05854b1818bdd15698eb20d10e42c
grace-official <76984418+grace-official@users.noreply.github.com>
2022-10-17 14:17:10 -0700

    e
    e

---
+++ finalProjectUnit1/Banking.py
@@ -0,0 +1,9 @@
+# bank balance checker
+class BankBalance:
+    # getter for bank balance
+    totalBalance = float(input("Enter your bank balance below: \n"))
+
+    # Item value
+    itemPrice = float(input("Enter the price of the item you wish to buy: \n"))
+
+    # calculating the amount you can purchase
