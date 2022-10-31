c221e1e398c17d66219e7edeb3e2ad8b90d7ea4b
Grace Egglestone <g.egglestone@surreyschools.ca>
2022-10-31 11:02:48 -0700

    Update Vehicles.py

--- Inheritance/Vehicles.py
+++ Inheritance/Vehicles.py
@@ -4,19 +4,41 @@ class Vehicle:
         self.numWheels = numWheels
         self.numPassengers = numPassengers
 
+    def drive(self):
+        print("Now driving")
+
+    def changeNumPassengers(self, newMax):
+        self.numPassengers = newMax
 
 class Car(Vehicle):
 
     def __init__(self, numPassengers):
         super().__init__(4, numPassengers)
 
+    def honkHorn(self):
+        print("Honk honk")
+
 class Motorcycle(Vehicle):
 
     def __init__(self):
         super().__init__(2, 1)
+        self.leanAmount = 60
+
+    def maxLeanAmount(self, maxLean):
+        self.leanAmount = maxLean
 
 car = Car(5)
 motorcycle = Motorcycle()
 
+print(car.numWheels)
+print(motorcycle.numWheels)
+
+car.drive()
+motorcycle.drive()
+
+car.changeNumPassengers(4)
+motorcycle.changeNumPassengers(2)
+
 car.honkHorn()
+motorcycle.leanAmount
 motorcycle.maxLeanAmount()
